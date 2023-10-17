b9c2a2ca064eefa39520557c119de255a1ab64ab
Margarita Nekrasova <nekr43@mail.ru>
2023-10-17 10:02:30 +0300

    Hi commit

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Hello"));
+                .body("data", equalTo("Hi"));
     }
 }
